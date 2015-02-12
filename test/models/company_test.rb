c5cc64b907e5a1cb82cd99734e5d46b0395fa882
itgsod-josef-dayne <josef.dayne@Josef-Dayne.local>
2015-02-12 11:13:07 +0100

    added company test

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -4,7 +4,23 @@ describe User do
 
   DataMapper.auto_migrate!
 
-  describe 'Name' do
+  describe 'crate a company' do
+
+    it 'should require name' do
+      expect( Company.count ).to eq 0
+      Company.create(name: 'valve', rating: 3, description: 'this is valve')
+      expect( Company.count ).to eq 1
+      Company.create(rating: 3, description: 'valve')
+      expect( Company.count ).to eq 1
+    end
+
+    it 'should require description' do
+      expect( Company.count ).to eq 0
+      Company.create(name: 'valve', rating: 3, description: 'this is valve')
+      expect( Company.count ).to eq 1
+      Company.create(name: 'this is valve', rating: 3)
+      expect( Company.count ).to eq 1
+    end
 
   end
 end
