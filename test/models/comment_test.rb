d1b954567e4db1615120c34b3b1d668e52912600
itgsod-josef-dayne <josef.dayne@Josef-Dayne.local>
2015-02-12 11:02:23 +0100

    Added comment tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,7 +4,31 @@ describe User do
 
   DataMapper.auto_migrate!
 
-  describe 'Name' do
+  describe 'create a comment' do
+
+    it 'should require type' do
+      expect( Comment.count ).to eq 0
+      Comment.create(type: 'user', comment: 'this is a game', points: 1)
+      expect( Comment.count ).to eq 1
+      Comment.create(comment: 'this is a game', points: 1)
+      expect( Comment.count ).to eq 1
+    end
+
+    it 'should require comment' do
+      expect( Comment.count ).to eq 0
+      Comment.create(type: 'user', comment: 'this is a game', points: 1)
+      expect( Comment.count ).to eq 1
+      Comment.create(type:'user', points: 1)
+      expect( Comment.count ).to eq 1
+    end
+
+    it 'should require points' do
+      expect( Comment.count ).to eq 0
+      Comment.create(type: 'user', comment: 'this is a game', points: 1)
+      expect( Comment.count ).to eq 1
+      Comment.create(type:'user', comment: 'this is a game')
+      expect( Comment.count ).to eq 1
+    end
 
   end
 end
