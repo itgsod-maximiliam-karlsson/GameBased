08cac1714cbeaa23428d283f49b6693df492f80d
itgsod-josef-dayne <josef.dayne@Josef-Dayne.local>
2015-02-12 10:01:23 +0100

    Game tests added

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -4,7 +4,47 @@ describe User do
 
   DataMapper.auto_migrate!
 
-  describe 'Name' do
+  describe 'create a game' do
+
+    it 'should require name' do
+      expect( Game.count ).to eq 0
+      Game.create(name: 'cunter stocked', description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+      Game.create(description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+    end
+
+    it 'should require description' do
+      expect( Game.count ).to eq 0
+      Game.create(name: 'cunter stocked', description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+      Game.create(description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+    end
+
+    it 'should require base_language_id' do
+      expect( Game.count ).to eq 0
+      Game.create(name: 'cunter stocked', description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+      Game.create(description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+    end
+
+    it 'should require release_date' do
+      expect( Game.count ).to eq 0
+      Game.create(name: 'cunter stocked', description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+      Game.create(description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+    end
+
+    it 'should require rating' do
+      expect( Game.count ).to eq 0
+      Game.create(name: 'cunter stocked', description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+      Game.create(description: 'this is a game', base_language_id: 1, release_date: '2015-11-11', rating: 1)
+      expect( Game.count ).to eq 1
+    end
 
   end
 end
