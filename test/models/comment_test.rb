5be398e6802b091e7291140183454eab418eaa9b
itgsod-maximiliam-karlsson <shrinkingcash@hotmail.com>
2015-02-12 11:08:18 +0100

    Fixed tests for comments

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -8,25 +8,25 @@ describe User do
 
     it 'should require type' do
       expect( Comment.count ).to eq 0
-      Comment.create(type: 'user', comment: 'this is a game', points: 1)
+      Comment.create(type: 'user', comment: 'this is a game', points: 1, game_id: 1, user_id: 1)
       expect( Comment.count ).to eq 1
-      Comment.create(comment: 'this is a game', points: 1)
+      Comment.create(comment: 'this is a game', points: 1, game_id: 1, user_id: 1)
       expect( Comment.count ).to eq 1
     end
 
     it 'should require comment' do
       expect( Comment.count ).to eq 0
-      Comment.create(type: 'user', comment: 'this is a game', points: 1)
+      Comment.create(type: 'user', comment: 'this is a game', points: 1, game_id: 1, user_id: 1)
       expect( Comment.count ).to eq 1
-      Comment.create(type:'user', points: 1)
+      Comment.create(type:'user', points: 1, game_id: 1, user_id: 1)
       expect( Comment.count ).to eq 1
     end
 
     it 'should require points' do
       expect( Comment.count ).to eq 0
-      Comment.create(type: 'user', comment: 'this is a game', points: 1)
+      Comment.create(type: 'user', comment: 'this is a game', points: 1, game_id: 1, user_id: 1)
       expect( Comment.count ).to eq 1
-      Comment.create(type:'user', comment: 'this is a game')
+      Comment.create(type:'user', comment: 'this is a game', game_id: 1, user_id: 1)
       expect( Comment.count ).to eq 1
     end
 
