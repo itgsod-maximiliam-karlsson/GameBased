f455d5a0ded4e5272f34870d6e8d5d6ab1989de5
itgsod-maximiliam-karlsson <shrinkingcash@hotmail.com>
2015-02-18 14:06:39 +0100

    Added interface test #2

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ class App < Sinatra::Base
   enable :sessions
 
   get '/' do
+    @comments = Comment.all
+    @companies = Company.all
+    @games = Game.all
+    @languages = Language.all
+    @roles = Role.all
+    @tags = Tag.all
+    @types = Type.all
     @users = User.all
     slim :index
   end
