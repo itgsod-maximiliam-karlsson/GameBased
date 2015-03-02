09fa2c3bdee5c9a2bd74347ffc8f7dd7c8062a01
itgsod-maximiliam-karlsson <shrinkingcash@hotmail.com>
2015-03-02 10:44:23 +0100

    Added Really good code for Josef to see

--- app.rb
+++ app.rb
@@ -23,26 +23,27 @@ class App < Sinatra::Base
     username = params['username'].capitalize
     email = params['email']
     get_password = params['password']
-    tands = params['termsandservice']
+    # tands = params['termsandservice']
 
-    if username.size < 3 or username.size > 15 or email.size < 5 or email.size > 32 or tands == nil
-      session[:error_msg] = 'Something went wrong'
-      redirect '/register'
-    else
-      if User.registered? username or User.registered_email? email
-        session[:error_msg] = 'Username or email is already in use'
-        redirect '/register'
-      else
-        user = User.create( username: username, email: email,  password: get_password)
-
-        session[:user_name] = user.username
-        session[:user_id] = user.id
-        session[:user_role] = user.role
-        LoginLog.create(time: Time.now, user_id: user.id)
-
-        redirect '/'
-      end
-    end
+    User.create( username: username, email: email,  password: get_password, role: Role.first)
+    # if username.size < 3 or username.size > 15 or email.size < 5 or email.size > 32 or tands == nil
+    #   session[:error_msg] = 'Something went wrong'
+    #   redirect '/register'
+    # else
+    #   if User.registered? username or User.registered_email? email
+    #     session[:error_msg] = 'Username or email is already in use'
+    #     redirect '/register'
+    #   else
+    #     user = User.create( username: username, email: email,  password: get_password)
+    #
+    #     session[:user_name] = user.username
+    #     session[:user_id] = user.id
+    #     session[:user_role] = user.role
+    #     LoginLog.create(time: Time.now, user_id: user.id)
+    #
+    #     redirect '/'
+    #   end
+    # end
   end
 
   get '/termsandservice' do
